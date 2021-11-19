72aed4fa1ba27cb3a6225c47133e509882dda6b1
Maria Raluca Diaconescu <44669618+MRDiaconescu@users.noreply.github.com>
2021-11-19 15:36:48 +0100

    Add files via upload

---
+++ Logarithm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+     class Logarithm
+    {
+        public string formula;
+        public int intVarX;
+        public double doubleVarX;
+        public int intResult;
+        public double doubleResult;
+        public int intVariableA;
+        public int intVariableB;
+        public int intVariableC;
+        public double doubleVariableA;
+        public double doubleVariableB;
+        public double doubleVariableC;
+        public int intVariableN;
+        public int intVariableK;
+    }
+}
